92fb8a01860b538778b6a83a5a7ee5d6ffe5bf0b
Pav1ikcel <119614428+Pav1ikcel@users.noreply.github.com>
2023-06-28 17:27:31 +0300

    Add files via upload

---
+++ program.java
@@ -0,0 +1,59 @@
+/*
+class Animal {
+    public void toGo() {
+        System.out.println("Животное двигается.");
+    }
+    
+    public void fly() {
+        System.out.println("Животное не умеет летать.");
+    }
+    
+    public void swim() {
+        System.out.println("Животное не умеет плавать.");
+    }
+}
+
+class Runner extends Animal {
+    public void toGo() {
+        System.out.println("Животное бегает.");
+    }
+}
+
+class Flyer extends Animal {
+    public void fly() {
+        System.out.println("Животное летит.");
+    }
+}
+
+class Swimmer extends Animal {
+    public void swim() {
+        System.out.println("Животное плавает.");
+    }
+}
+
+public class AnimalMain {
+    public static void main(String[] args) {
+        Animal animal1 = new Animal();
+        Animal runner = new Runner();
+        Animal flyer = new Flyer();
+        Animal swimmer = new Swimmer();
+        
+        animal1.toGo(); // Животное двигается.
+        animal1.fly(); // Животное не умеет летать.
+        animal1.swim(); // Животное не умеет плавать.
+        
+        runner.toGo(); // Животное бегает.
+        runner.fly(); // Животное не умеет летать.
+        runner.swim(); // Животное не умеет плавать.
+        
+        flyer.toGo(); // Животное двигается.
+        flyer.fly(); // Животное летит.
+        flyer.swim(); // Животное не умеет плавать.
+        
+        swimmer.toGo(); // Животное двигается.
+        swimmer.fly(); // Животное не умеет летать.
+        swimmer.swim(); // Животное плавает.
+    }
+}
+
+ */
